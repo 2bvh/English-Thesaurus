dc5b9dae8f465beaaf4bdc25b3fccf9d6b9ad41d
Brian Harney <brian.r.harney@gmail.com>
2021-06-23 16:56:16 -0500

    Make output more user friendly

--- thesaurus.py
+++ thesaurus.py
@@ -1,15 +1,32 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
 
 def get_definition(w):
+    w = w.lower()
     if w in data:
         return data[w]
+    elif len(get_close_matches(w, data.keys())) > 0:
+        top_match = get_close_matches(w, data.keys())[0]
+        yn = input("Did you mean %s? Enter Y/N " % top_match)
+        if yn == "Y":
+            return data[get_close_matches(w, data.keys())[0]]
+        elif yn == "N":
+            return "Please try again"
+        else:
+            return "We didn't understand your response."
     else:
         return "That word doesn't exist."
 
 
-word = input("Enter word: ").lower()
+word = input("Enter word: ")
 
-print(get_definition(word))
+output = get_definition(word)
+
+if type(output) == list:
+    for item in list:
+        print(item)
+else:
+    print(output)
