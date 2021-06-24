116c9ae038928635a8e8faf60993644a01c4d5d8
Brian Harney <brian.r.harney@gmail.com>
2021-06-24 11:45:21 -0500

    Add acronym funtionality

--- thesaurus.py
+++ thesaurus.py
@@ -8,6 +8,10 @@ def get_definition(w):
     w = w.lower()
     if w in data:
         return data[w]
+    elif w.capitalize() in data:
+        return data[w.capitalize()]
+    elif w.upper() in data:
+        return data[w.upper()]
     elif len(get_close_matches(w, data.keys())) > 0:
         top_match = get_close_matches(w, data.keys())[0]
         yn = input("Did you mean %s? Enter Y/N " % top_match)
@@ -26,7 +30,7 @@ word = input("Enter word: ")
 output = get_definition(word)
 
 if type(output) == list:
-    for item in list:
+    for item in output:
         print(item)
 else:
     print(output)
